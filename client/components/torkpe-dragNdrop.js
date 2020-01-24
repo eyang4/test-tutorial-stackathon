c6803cd97dd8bc46babc70a4ce7377f521d79d1c
Eric Yang <yangeric12@gmail.com>
2020-01-24 15:48:09 -0500

    not DRY but functionally sound drag and drop with positioning and without placement

--- client/components/torkpe-dragNdrop.js
+++ client/components/torkpe-dragNdrop.js
@@ -16,12 +16,22 @@ class TorkPeDragNDrop extends Component {
         {
           taskID: 3,
           task: 'Jump the jump'
+        },
+        {
+          taskID: 4,
+          task: 'Hop the hop'
+        },
+        {
+          taskID: 5,
+          task: 'Skip the skip'
         }
       ],
       completedTasks: [],
       draggedTask: {}
     }
   }
+
+  // as soon as a div element is dragged, store contents
 
   onDrag = (event, todoOrTask) => {
     event.preventDefault()
@@ -30,27 +40,90 @@ class TorkPeDragNDrop extends Component {
     })
   }
 
+  // used to determine what feedback is to be shown to the user
+
   onDragOver = event => {
     event.preventDefault()
   }
 
-  onDrop = (event, location) => {
-    const {completedTasks, draggedTask, todos} = this.state
-    if (location === 'tasks') {
-      this.setState({
-        completedTasks: [...completedTasks, draggedTask],
-        todos: todos.filter(task => task.taskID !== draggedTask.taskID),
-        draggedTask: {}
-      })
-    } else if (location === 'todos') {
-      this.setState({
-        todos: [...todos, draggedTask],
-        completedTasks: completedTasks.filter(
-          task => task.taskID !== draggedTask.taskID
-        ),
-        draggedTask: {}
-      })
+  // when a div element is dropped, update state (which causes re-render)
+
+  onDrop = (event, parentDiv, index) => {
+    console.log('index:', index, ', parentDiv:', parentDiv)
+    if (index !== undefined) {
+      const {completedTasks, draggedTask, todos} = this.state
+      if (parentDiv === 'tasks') {
+        if (
+          !Object.values(completedTasks)
+            .map(elem => elem.taskID)
+            .includes(draggedTask.taskID)
+        ) {
+          this.setState({
+            // completedTasks: [...completedTasks, draggedTask],
+            completedTasks: [
+              ...completedTasks.slice(0, index),
+              draggedTask,
+              ...completedTasks.slice(index)
+            ],
+            todos: todos.filter(task => task.taskID !== draggedTask.taskID),
+            draggedTask: {}
+          })
+        }
+        event.stopPropagation() // prevent parent from calling onDrop
+      } else if (parentDiv === 'todos') {
+        if (
+          !Object.values(todos)
+            .map(elem => elem.taskID)
+            .includes(draggedTask.taskID)
+        ) {
+          this.setState({
+            // todos: [...todos, draggedTask],
+            todos: [
+              ...todos.slice(0, index),
+              draggedTask,
+              ...todos.slice(index)
+            ],
+            completedTasks: completedTasks.filter(
+              task => task.taskID !== draggedTask.taskID
+            ),
+            draggedTask: {}
+          })
+        }
+        event.stopPropagation() // prevent parent from calling onDrop
+      }
+    } else {
+      const {completedTasks, draggedTask, todos} = this.state
+      if (parentDiv === 'tasks') {
+        if (
+          !Object.values(completedTasks)
+            .map(elem => elem.taskID)
+            .includes(draggedTask.taskID)
+        ) {
+          this.setState({
+            completedTasks: [...completedTasks, draggedTask],
+            todos: todos.filter(task => task.taskID !== draggedTask.taskID),
+            draggedTask: {}
+          })
+        }
+        event.stopPropagation() // prevent parent from calling onDrop
+      } else if (parentDiv === 'todos') {
+        if (
+          !Object.values(todos)
+            .map(elem => elem.taskID)
+            .includes(draggedTask.taskID)
+        ) {
+          this.setState({
+            todos: [...todos, draggedTask],
+            completedTasks: completedTasks.filter(
+              task => task.taskID !== draggedTask.taskID
+            ),
+            draggedTask: {}
+          })
+        }
+        event.stopPropagation() // prevent parent from calling onDrop
+      }
     }
+    console.log('state', this.state)
   }
   render() {
     const {todos, completedTasks} = this.state
@@ -62,11 +135,13 @@ class TorkPeDragNDrop extends Component {
           className="todos"
         >
           Todo
-          {todos.map(todo => (
+          {todos.map((todo, index) => (
             <div
               key={todo.taskID}
               draggable
+              onDrop={event => this.onDrop(event, 'todos', index)}
               onDrag={event => this.onDrag(event, todo)}
+              className="taskDiv"
             >
               {todo.task}
             </div>
@@ -82,7 +157,9 @@ class TorkPeDragNDrop extends Component {
             <div
               key={task.taskID}
               draggable
+              onDrop={event => this.onDrop(event, 'tasks', index)}
               onDrag={event => this.onDrag(event, task)}
+              className="taskDiv"
             >
               {task.task}
             </div>
